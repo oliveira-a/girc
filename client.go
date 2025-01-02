7267e91dba9b034a80cfc8f2330122f27dd087d8
André Brasil <andremiguelbrasil@gmail.com>
2025-01-02 21:53:49 +0000

    feat: add alias

--- client.go
+++ client.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+var hostFlag = flag.String("h", "", "the irc host. i.e. localhost")
+var portFlag = flag.String("p", "", "the irc server port number. i.e. 2000.")
+var aliasFlag = flag.String("a", "", "an alias. i.e. andreb")
+
+func init() {
+	flag.Parse()
+}
 
 func main() {
-	fmt.Println("Hello, World (from client)!")
+	c, err := net.Dial("tcp", *hostFlag+":"+*portFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    r := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print(*aliasFlag + "> ")
+        m, _ := r.ReadString('\n')
+
+        fmt.Fprintf(c, m)
+    }
 }
